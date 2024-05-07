49c43b24bd1c3661b1c4e0c42d3cc0ecd94145a6
chiarafais <chiara0915@gmail.com>
2024-05-07 14:54:22 +0200

    finished exercise after understanding yesterday's solution

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -9,11 +9,23 @@ const reducers = (state = initialState, action) => {
     case "ADD_TO_FAVOURITES":
       return {
         ...state,
-        favourites: { content: [...state.favourites.content, action.payload] },
+        favourites: {
+          ...state.favourites,
+          content: [...state.favourites.content, action.payload],
+        },
       };
 
+    case "REMOVE_FROM_FAVOURITES":
+      return {
+        ...state,
+        favourites: {
+          ...state.favourites,
+          content: state.favourites.content.filter((fav) => fav !== action.payload),
+        },
+      };
     default:
       return state;
   }
 };
+
 export default reducers;
